03de288a2c2de022c8c9f916262cf9983cbc80d9
fernandasinara <141151372+fernandasinara@users.noreply.github.com>
2025-05-26 09:15:36 -0300

    .

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const caixaprincipal = document.querySelector(caixa principal)
-const caixaPerguntas = document
-const caixaAlternativa
-const caixaResultados= document.querySelector
-const perguntas =[
-{
+{const caixaPrincipal = document.querySelector("caixa-principal");
+const caixaPerguntas = document.querySelector("caixa-perguntas");
+const caixaAlternativas = document.querySelector("caixa-alternativas");
+const caixaResultado = document.querySelector("caixa-resultado");
+const textoResultado = document.querySelector("texto-resultado");
+const perguntas = [
 nunciado: 'pergunta 1',
 alternativas: ["alternativa1","alternativa2"]
 },
